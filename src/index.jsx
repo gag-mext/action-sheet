c7bd760fef1d95043bf90bce191539cfae19a179
姚嘉松 <yaojiasong@gooagoo.com>
2017-11-01 17:34:18 +0800

    组件转移 无线 UI 组件 action-sheet

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/**
+ * ActionSheet Component
+ * @Author: litongqian
+ * @Date:   2017-03-15 16:28:25
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+module.exports = require('./ActionSheet');
